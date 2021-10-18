98dbb13c38170146f48b11aab12cc448770e7167
MakarenkoEvgen <evgenijmakarenko036@gmail.com>
2021-10-18 15:15:24 +0300

    begin to work

---
+++ README.md
@@ -0,0 +1,10 @@
+# Webpack template
+
+```powershell
+To start build:
+
+npm ci (to start project)
+
+npm run watch   (for start watching the project)
+npm run build   (for build project once with minification of .js and .css files)
+```
---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const config = {
+    entry: ["./src/js/index.js", "./src/scss/main.scss"],
+    output: {
+        path: __dirname + '/dist',
+        filename: "js/main.min.js"
+    },
+    devtool: "source-map",
+    mode: "production",
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true,
+                extractComments: true
+            })
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, "./src/scss"),
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            url: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            postcssOptions: {
+                                plugins: [['autoprefixer', { }]]
+                            }
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader",
+                    options: {
+                        attrs: ['img:src','link:href','source:srcset']
+                    }
+                }
+            },
+            {
+                test: /\.(svg|png|jpg|eot|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "./images"
+                    }
+                },
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/style.css"
+        }),
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: "./src/index.html"
+        }),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            pngquant: {
+                quality: '80'
+            },
+            jpegtran: {
+                quality: '80',
+                progressive: true
+            }
+        })
+    ]
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === "production") {
+        config.plugins.push(new CleanWebpackPlugin());
+    }
+    return config;
+};
