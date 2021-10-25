dfe7f226eb05c2592ddebe546fee9cf075b1ea48
NektoStas <air@MacBook-Air.local>
2021-10-25 15:47:45 +0300

    without responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
 window.$ = window.jQuery = require('jquery');
+
+
+
+$(document).ready(function(){
+    $('.header').on('click', '.header__mobile-menu, .navigation__close', function(){
+        $('.navigation').slideToggle();
+    })
+
+    const reviews = new Swiper('.reviews__slider', {
+        loop: true,
+      
+        pagination: {
+          el: '.swiper-pagination',
+        },
+       
+      });
+
+});
+
+$(window).resize(function() {
+    if($(window).width()>=768) {
+        $('.navigation').attr('style', '');
+     }
+ })
+ 
+ $(window).scroll(function(){
+     const header = $('.header');
+     const windowPosition = $(window).scrollTop();
+     const bannerHeight = $('#banner').outerHeight();
+     
+     
+     if(windowPosition >= bannerHeight - header.outerHeight()){
+        header.addClass('stiky');
+     }
+     else {
+        header.removeClass('stiky');
+     }
+ })
